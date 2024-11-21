684d54c0f7ddc0c52b3f0465b3edae40d3705f0b
Evellyn Valencia <Evellyn_2701@hotmail.com>
2024-11-21 01:16:56 -0300

    Estilizando a tela Home

--- src/css/HomeStyle.jsx
+++ src/css/HomeStyle.jsx
@@ -107,6 +107,43 @@ export const HomeStyle=styled.div`
     object-fit: cover;
 }
 
+.info3 {
+    display: flex;
+    align-items: center;
+    color: #fff;
+    padding: 40px;
+    position: relative;
+}
+
+.text-content3 h2 {
+    font-size: 2rem;
+    color: #CBE86A;
+    margin-bottom: 40px;
+}
+
+.text-content3 h3 {
+    font-size: 1.5rem;
+    max-width: 60%;
+    text-align: left;
+    margin-top: 20px;
+    margin-bottom: 10px;
+}
+
+.text-content3 p {
+    max-width: 60%;
+    font-size: 1rem;
+    line-height: 1.6;
+    text-align: left;
+    margin-bottom: 20px;
+}
+
+.img-info3 {
+    max-width: 100%;
+    height: 500px;
+    position: absolute; 
+    right: 0; 
+    top: 150px; 
+}
 
 
 `
